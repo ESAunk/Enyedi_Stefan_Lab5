f7a2dfd17d0fa356f5af8514a42b0fdd898e90ed
Stefan Enyedi <enyedistefan@hotmail.com>
2021-11-06 18:56:02 +0200

    Add project files.

---
+++ dbo.Order.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Order] (
+    [OrderId] INT IDENTITY (1, 1) NOT NULL,
+    [CustId]  INT NULL,
+    [CarId]   INT NULL,
+CONSTRAINT fk_orders_cust_id FOREIGN KEY (CustId) REFERENCES Customer (CustId) ON DELETE CASCADE ON UPDATE CASCADE,
+CONSTRAINT fk_orders_inv_id FOREIGN KEY (CarId) REFERENCES Inventory (CarId) ON DELETE CASCADE ON UPDATE CASCADE
+)
+
---
+++ dbo.Table_1.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[OrderId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [CustId] INT NULL, 
+    [CarId] INT NULL
+)
